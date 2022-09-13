f37c76b7e5d83717e5061ccf44ee1648238b41ef
ShanmukhSaiMadhu <madhushanmukhsai@gmail.com>
2022-09-13 22:21:53 +0530

    link added

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ function Header() {
   return (
     <header>
         <img src='https://iconape.com/wp-content/png_logo_vector/troll-face-logo.png' className='header-image' />
-        <h1 className="header-title">Meme Generato</h1>
+        <h1 className="header-title">Meme Generator</h1>
     </header>
   )
 }
